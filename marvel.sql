10d3fd99c4e8d7692283a3cf9525ae9a8dc08db0
Pedro Everett <pedrojoseeverett@gmail.com>
2017-06-05 16:12:30 +0100

    update by id

--- marvel.sql
+++ marvel.sql
@@ -62,8 +62,12 @@ SELECT name FROM people WHERE name = 'Pedro Everett';
 -- 5. The cinema is showing 'Batman Begins', but Batman is DC, not Marvel! Delete the entry from the 'movies' table.
 DELETE FROM movies WHERE title = 'Batman Begins';
 -- 6. Create a new entry in the 'people' table with the name of one of the instructors.
+INSERT INTO people (name) VALUES ('Zsolt');
 -- 7. Winston Ingram, has decided to hijack our movie evening, Remove him from the table of people.
+DELETE FROM people WHERE name = 'Winston Ingram';
 -- 8. Somehow the list of people includes two people named 'Fred'. Change these entries to the proper names ('Jack Jarvis', 'Victor McDade')
+UPDATE people SET name = 'Jack Jarvis' WHERE id = 9;
+UPDATE people SET name = 'Victor McDade' WHERE id = 13;
 -- 9. The cinema has just heard that they will be holding an exclusive midnight showing of 'Guardians of the Galaxy 2'!! Create a new entry in the 'movies' table to reflect this.
 -- 10. The cinema would also like to make the Guardian movies a back to back feature. Update the 'Guardians of the Galaxy' show time from 12:10 to 21:30
 
